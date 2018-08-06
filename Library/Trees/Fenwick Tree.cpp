789739076f2b2b6879991f9b0b0356f9b832f65a
Leonardo Rodrigues <leoonardodf@gmail.com>
2018-08-06 17:23:51 -0300

    Fenwick tree

--- Library/Trees/Fenwick Tree.cpp
+++ Library/Trees/Fenwick Tree.cpp
@@ -1,8 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define LOG2(X)         ((unsigned) (8*sizeof(unsigned long long) - __builtin_clzll((X)) - 1))
+
+/** Fenwick Tree, by Leonardo Rodrigues.
+ * Query: O(log N);
+ * Update: O(log N);
+ * 
+ * neutral = Elemento neutro dentro da estrutura;
+ * f = Função de acumulação de valores dentre intervalos;
+ * u = Função de atualização de valores dentre os intervalos.
+ * 
+ * Utilização:
+ * * FenwickTree<T> aux(N, neutral, T (T a, T b){ return a + b; }, T (T a, T b){ return a + b; });
+*/
+template <class T> class FenwickTree{
+    int n;
+    T neutral;
+    vector<T> ft;
+    auto (*f)(T, T) -> T;
+    auto (*u)(T, T) -> T;
+    
+    public:
+        FenwickTree(int n, T neutral, T (*f)(T, T), T (*u)(T, T)): n(n){
+            ft.assign(n + 1, neutral);
+            this->f = f, this->u = u;
+        };
+        FenwickTree(vector<int>& e, T neutral, T (*f)(T, T), T (*u)(T, T)): n(e.size()){
+            ft.assign(n + 1, neutral);
+            this->f = f, this->u = u;
+            for(int i = 0; i < n; i++)
+                this->update(i, e[i]);
+        };
+        T query(int i){
+            T ans = this->neutral;
+            for(i++; i > 0; i -= (i & -i))
+                ans = this->f(ans, this->ft[i]);
+            return ans;
+        }
+        T query(int i, int j){
+            return this->query(j) - this->query(i - 1);
+        }
+        void update(int i, T x){
+            for(i++; i <= this->n; i += (i & -i))
+                this->ft[i] = this->u(this->ft[i], x);
+        };
+};
+
+int f(int a, int b){
+    return a + b;
+}
 
 int main(){
-    
+    int n, q;
+    scanf("%d %d", &n, &q);
+    FenwickTree<int> aux(n, 0, f, f);
+    for(int i = 0; i < n; i++){
+        int a;
+        scanf("%d", &a);
+        aux.update(i, a);
+    }
+    for(int i = 0; i < q; i++){
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+        if(a == 1) printf("{%d, %d} => %d\n", b, c, aux.query(b, c));
+        else aux.update(b, c);
+    }
     return 0;
 }
