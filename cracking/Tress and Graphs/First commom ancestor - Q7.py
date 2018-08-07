93c6bc3db33efb043d6e01df5fa49edc84e6041e
Leonardo Rodrigues <leoonardodf@gmail.com>
2018-08-07 10:25:12 -0300

    Rename folders and files

---
+++ cracking/Tress and Graphs/First commom ancestor - Q7.py
@@ -0,0 +1,6 @@
+'''
+4.8 - Tress and Graphs:
+Design an algorithm and write code to find the first commom ancestor of two
+nodes in binary tree. Avoid storing additional nodes in a data structure. 
+NOTE: this is not necessarily a binary search tree.
+'''
