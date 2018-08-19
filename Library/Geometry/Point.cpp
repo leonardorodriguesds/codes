e963b544b6251160f6f0d8d6336da4b8a62179a6
Leonardo Rodrigues <leoonardodf@gmail.com>
2018-08-19 16:25:12 -0300

    Point update

--- Library/Geometry/Point.cpp
+++ Library/Geometry/Point.cpp
@@ -9,18 +9,19 @@ bool equals(double a, double b){
 typedef struct _Point{
     double x, y;
     _Point(double x, double y): x(x), y(y){};
-    bool operator==(Point& other){
+    bool operator==(_Point& other){
         return equals(this->x, other.x) && equals(this->y, other.y);
     };
-    bool operator<(Point& other){
+    bool operator<(_Point& other){
         return (equals(this->x, other.x)) ? (this->y - other.y) > EPS : this->x - other.x > EPS;
     };
-    double distance(Point& other){
+    double distance(_Point& other){
         return hypot(this->x - other.x, this->y - other.y);  
     };
 } Point;
 
 int main(){
-    
+    auto a = Point(4.1, 5.1), b = Point(4.1, 5.1);
+    if(a == b) printf("s\n");
     return 0;
 }
