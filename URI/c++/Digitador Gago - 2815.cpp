af7072365f72a7f89f2b77a20a443de25bdb0257
leonardorodriguesds <leoonardodf@gmail.com>
2023-08-05 12:19:19 -0300

    lca e binary lifting

---
+++ URI/c++/Digitador Gago - 2815.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ALFA            256
+#define MOD             1000000007
+#define INF             0x3f3f3f3f
+#define INFLL           0x3f3f3f3f3f3f3f3f
+#define EPS             (1e-9)
+#define PI              3.141592653589793238462643383279502884
+#define all(a)          a.begin(), a.end()
+#define fill(t,v)       memset(t, v, sizeof(t))
+#define sz(a)           ((int)(a.size()))
+#define LOG2(X)         ((unsigned) (8*sizeof(unsigned long long) - __builtin_clzll((X)) - 1))
+#define ispow2(v)       ((int(v) & (int(v) - 1)) == 0)
+#define scanf2(a, b)    (scanf("%d %d", &a, &b))
+#define scanf3(a, b, c) (scanf("%d %d %d", &a, &b, &c))
+#define FOR(x,n) for(int x = 0; (x)<int(n); (x)++)
+#define FOR1(x,n) for(int x = 1; (x)<=int(n); (x)++)
+#define REP(x,n) for(int x = int(n)-1; (x)>=0; (x)--)
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<pair<int, ii>> vpii;
+typedef vector<string> vs;
+typedef priority_queue<int, vector<int>, greater<int>> pqi;
+typedef vector<pqi> vpqi;
+
+string solveIt(string word) {
+    if (word.size() > 3) {
+        string primeira_silaba = word.substr(0,2);
+        while (word.substr(2,2).compare(primeira_silaba) == 0) {
+            word.erase(0, 2);
+        }
+        return word;
+    }
+    return word;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); 
+    string word;
+    auto space = false;
+    while (cin >> word) {
+        cout << (space ? " " : "") << solveIt(word);
+        space = true;
+    }
+    return 0;
+}
